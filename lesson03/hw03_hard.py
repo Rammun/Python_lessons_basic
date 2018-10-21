5d3b50bd0bf5ab1df46326ecdc56d81242d7cdb2
erka <rlikh75@mail.ru>
2018-10-21 03:35:24 +0300

    hard

--- lesson03/hw03_hard.py
+++ lesson03/hw03_hard.py
@@ -9,6 +9,7 @@
 # Ввод: -2/3 - -2
 # Вывод: 1 1/3
 
+# время уже жалко
 
 # Задание-2:
 # Дана ведомость расчета заработной платы (файл "data/workers").
@@ -18,6 +19,47 @@
 # они получают удвоенную ЗП, пропорциональную норме.
 # Кол-во часов, которые были отработаны, указаны в файле "data/hours_of"
 
+import os
+
+dir_in = "data"
+dir_out = "data\\out"
+
+def read_data_from_file(path):
+	result = []
+	with open(path, 'r', encoding='UTF-8') as f:
+		first_line = True		
+		for line in f:
+			if first_line == True:
+				first_line = False
+				continue		
+			if(len(line) > 0 and line != "\n"):
+				row = list(filter(lambda x: x != "" and x != "\n", line.replace("\n", "").split(" ")))
+				result.append(row)
+	return result
+
+workers = {}
+result = read_data_from_file(os.path.join(dir_in, "workers"))
+for row in result:
+	workers[(row[0], row[1])] = { "salary": int(row[2]), "position": row[3], "rate": int(row[4]) }
+
+hours = {}
+result = read_data_from_file(os.path.join(dir_in, "hours_of"))
+for row in result:
+	hours[(row[0], row[1])] = int(row[2])
+
+with open(os.path.join(dir_out, "register.txt"), 'a', encoding='UTF-8') as f:
+	f.write("Имя         Фамилия     Зарплата\n\n")
+	for fullname in hours:
+		if(workers.get(fullname) != None):
+			employee = workers[fullname]
+			cost_one_hour = employee["salary"] / employee["rate"]
+			excess = hours[fullname] - employee["salary"]
+			if(excess > 0):
+				current_salary = employee["rate"] * cost_one_hour + excess * cost_one_hour * 2
+			else:
+				current_salary = hours[fullname] * cost_one_hour
+			row = f"{fullname[0]:<12}{fullname[1]:<12}{current_salary:.2f}\n"
+			f.write(row)
 
 # Задание-3:
 # Дан файл ("data/fruits") со списком фруктов.
@@ -31,3 +73,27 @@
 # Подсказка:
 # Чтобы получить список больших букв русского алфавита:
 # print(list(map(chr, range(ord('А'), ord('Я')+1))))
+
+import os
+
+dir_in = "data"
+dir_out = "out"
+filename = "fruits.txt"
+path = os.path.join(dir_in, filename)
+data = {}
+
+with open(path, 'r', encoding='UTF-8') as f:
+	for line in f:
+		if(len(line) > 0 and line != "\n"):
+			first_letter = line[0].upper()
+			if(data.get(first_letter) == None):
+				data[first_letter] = []
+			data[first_letter].append(line)
+
+for key in data:
+	_filename = f"fruits_{key}.txt"
+	with open(os.path.join(dir_in, dir_out, _filename), 'a', encoding='UTF-8', ) as f:
+		for line in data[key]:
+			f.write(line)
+
+
